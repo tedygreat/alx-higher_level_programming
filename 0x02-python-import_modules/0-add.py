936876e961a98374a5a4c5305921cef890cfbf87
tedygreat <tedygreat@gmail.com>
2023-03-09 12:11:34 -0800

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
+if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print(f"{a:d} + {b:d} = {add(a, b):d}")
+    print(f"{a} + {b} = {add(a, b)}")
