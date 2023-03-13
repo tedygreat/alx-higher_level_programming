4e792a7f2b9727bf23d505f371f4060124ddd234
tedygreat <tedygreat@gmail.com>
2023-03-13 14:21:40 -0700

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     listofchars = list(my_string)
-    for char in listofchars:
-        if (char == 'c' or char == 'C'):
-            listofchars.remove(char)
-    return("".join(listofchars))
+    new = ""
+    for char in my_string:
+        if (char != 'c' or char != 'C'):
+            new += char
+    return(new)
