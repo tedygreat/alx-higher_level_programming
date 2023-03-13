04af4914edbbbefa78320ab8e9ff88fba9417863
tedygreat <tedygreat@gmail.com>
2023-03-13 14:59:24 -0700

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,10 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-           print("{:d}".format(j), end = "")
+            x = 1
+            if x == len(i):
+                print("{:d}".format(j), end="")
+            else:
+                print("{:d}".format(j), end=" ")
+            x += 1
         print()
