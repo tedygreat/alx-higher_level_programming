c71f03d6654b1f37706f9229b8d33318487adf5e
tedygreat <tedygreat@gmail.com>
2023-03-15 13:50:06 -0700

    task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = set(my_list) # it remove redendont item and orderd
+    new_list = set(my_list)
     num = 0
     for i in new_list:
         num += i
