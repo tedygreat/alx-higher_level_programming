59b090a4ee261e7f3a41f6873eabc1bd98853cea
tedygreat <tedygreat@gmail.com>
2023-03-15 13:06:41 -0700

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            return (new_list[i] = replace)
+        else:
+            return (new_list[i] = my_list[i])
