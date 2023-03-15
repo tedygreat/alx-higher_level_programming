5521ca7dfd75042c7ac5a2d55bd789c037aef79f
tedygreat <tedygreat@gmail.com>
2023-03-15 13:21:03 -0700

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     for i in range(len(my_list)):
         if my_list[i] == search:
-            return (new_list[i] = replace)
+            new_list[i] = replace
         else:
-            return (new_list[i] = my_list[i])
+            new_list[i] = my_list[i]
+    return (new_list)
