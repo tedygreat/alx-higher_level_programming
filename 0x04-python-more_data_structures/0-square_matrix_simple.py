16bdd692d780e3b556b161f6acfb7236a59f487c
tedygreat <tedygreat@gmail.com>
2023-03-15 12:54:48 -0700

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,4 @@ def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-    return (new__matrix)
-
-
+    return (new_matrix)
