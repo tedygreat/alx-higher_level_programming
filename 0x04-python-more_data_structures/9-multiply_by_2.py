b064477a7682e553e53ed1fa01a75a5aaa5c87cd
tedygreat <tedygreat@gmail.com>
2023-03-15 14:33:25 -0700

    task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dic = a_dictionary.copy()
+    list_key =list(new_dec.keys())
+    for i in list_key:
+        new_dic[i] *= 2
+    return (new_dic)
