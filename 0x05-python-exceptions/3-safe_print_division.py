85bbd7912ad56cc7726a8759b5d9880fbfff170a
tedygreat <tedygreat@gmail.com>
2023-03-27 14:11:37 -0700

    task3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,7 +8,7 @@ def safe_print_division(a, b):
     """
     try:
         res = a / b
-    except:
+    except (TypeError, ZeroDivisionError):
         res = None
     finally:
         print("Inside result: {}".format(res))
