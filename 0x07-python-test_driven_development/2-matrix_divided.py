b8370782452b7de157a8ff3cefffe765cded3302
tedygreat <tedygreat@gmail.com>
2023-04-06 12:29:29 -0700

    task

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 
 
-""" matrix """
-
-
+""" mat """
 def matrix_divided(matrix, div):
-    """ matrix """
-    errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
-    if not matrix:
-        raise TypeError(errorMessage)
-    if not isinstance(matrix, list):
-        raise TypeError(errorMessage)
-    for lists in matrix:
-        if not isinstance(lists, list):
-            raise TypeError(errorMessage)
-        for item in lists:
-            if not isinstance(item, int) and not isinstance(item, float):
-                raise TypeError(errorMessage)
-    for lists in matrix:
-        if len(lists) == 0:
-            raise TypeError(errorMessage)
-    if not isinstance(div, int) and not isinstance(div, float):
+    """
+    divides all elements of a matrix, elements must be ->
+        int or float type
+    """
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    if not all(len(lists) == len(matrix[0]) for lists in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-    if div == 0:
+        return matrix
+    elif div == 0:
         raise ZeroDivisionError("division by zero")
-    return [[round(item / div, 2) for item in lists] for lists in matrix]
+        return matrix
+
+    prevRowLen = -1
+    new_list = []
+    for row in matrix:
+        if (prevRowLen != len(row) and prevRowLen != -1):
+            raise TypeError("Each row of the matrix must have the same size")
+            return matrix
+        for ele in row:
+            if not isinstance(ele, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of" +
+                                " integers/floats")
+                return matrix
+            else:
+                new_list.append(round(ele / div, 2))
+        prevRowLen = len(row)
+
+    return new_list
