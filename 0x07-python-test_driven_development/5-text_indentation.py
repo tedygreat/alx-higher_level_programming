3e99ed9abefb60d49949becf7080860c693d45c8
tedygreat <tedygreat@gmail.com>
2023-04-06 12:52:25 -0700

    task

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -18,6 +18,3 @@ def text_indentation(text):
         if ch in specials:
             string += "\n\n"
     print(string, end='')
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/5-text_indentation.txt")
