2f7f9d9b2c0637913cd32144700b081519c81c5a
tedygreat <tedygreat@gmail.com>
2023-04-06 12:37:58 -0700

    task

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-
-
-""" txt """
+"""text indent"""
 
 
 def text_indentation(text):
-    """
-    prints a string of text with 2 new lines after '.', '?', and ':'
-    unit tests located in tests/5-text_indentation.txt
-    """
+    """text indent"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    string = ""
-    specials = ['.', '?', ':']
-    for ch in text:
-        string += ch
-        if ch in specials:
-            string += "\n\n"
-    print(string, end='')
+    for delimeter in "?:.":
+        words = (delimeter + "\n\n").join(
+                [index.strip(" ") for index in words.split(delimeter)])
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
