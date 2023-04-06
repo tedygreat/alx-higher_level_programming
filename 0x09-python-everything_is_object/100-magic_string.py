d5ef2a93b0bad0236153f83a98c32c7508fc803a
tedygreat <tedygreat@gmail.com>
2023-04-06 14:04:23 -0700

    task

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
-def magic_string():
-    from counter import Counter
-    Counter.i += 1
-    return ", ".join(["BestSchool" for i in range(0, Counter.i)])
+#!/usr/bin/python3
+def magic_string(Holby=[]):
+    Holby += ["BestSchool"]
+    return (", ".join(Holby))
