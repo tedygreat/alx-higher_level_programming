73029d03c042fef17b4c052bc362c6a1677fc898
tedygreat <tedygreat@gmail.com>
2023-04-10 15:37:42 -0700

    task

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -11,8 +11,8 @@ class Square(Rectangle):
     def __init__(self, size):
         """instantiation method for class
         """
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
