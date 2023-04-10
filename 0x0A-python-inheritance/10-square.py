1da5a35c2f0d4f906077b6f74ed38b36d46c8d2a
tedygreat <tedygreat@gmail.com>
2023-04-10 15:09:59 -0700

    tas 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,6 +9,6 @@ class Square(Rectangle):
     """ square shape class, super class is BaseGeometry, then Rectangle
     """
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
