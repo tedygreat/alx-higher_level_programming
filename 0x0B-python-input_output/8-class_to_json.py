8f5e50a2932b5cff230615f1b9dc594883f628c5
tedygreat <tedygreat@gmail.com>
2023-04-11 14:53:27 -0700

    task 7

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -8,5 +8,4 @@ def class_to_json(obj):
     """creates a json representation of an object without using
         json module. One liner!! (without pep8 fix) :D
     """
-    return repr({key: value for (key, value) in obj.__dict__.items()
-                if key in list(obj.__dict__.keys())})
+    return vars(obj)
