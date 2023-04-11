5e0f3e59f75c4a83fe83a6599289f5acef1163b2
tedygreat <tedygreat@gmail.com>
2023-04-11 15:03:48 -0700

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -5,32 +5,23 @@
 
 
 class Student:
-    """student class for use
-    """
+    """Student obj, interesting how you don't have to directly
+    test for strings in a loop, python is weird"""
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves dict rep of Student in json form
-        """
-        is_all_strs = True
-        if isinstance(attrs, list):
-            for ele in attrs:
-                if not isinstance(ele, str):
-                    is_all_strs = False
-        else:
-            is_all_strs = False
-
-        return {key: value for (key, value) in self.__dict__.items()
-                if key in list(self.__dict__.keys()) and
-                (not is_all_strs or key in attrs)}
+        if attrs is None:
+            return self.__dict__
+        new_dictionary = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dictionary[key] = value
+        return new_dictionary
 
     def reload_from_json(self, json):
-        """reloads an object from a json dictionary
-            -> assumes json to always be a dictionary
-        """
-        self.first_name = json['first_name']
-        self.last_name = json['last_name']
-        self.age = json['age']
+        for key, value in json.items():
+            setattr(self, key, value)
