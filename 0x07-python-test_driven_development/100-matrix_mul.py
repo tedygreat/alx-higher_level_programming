13fd5cab29b4cc7256937336789a27957816b2ab
tedygreat <tedygreat@gmail.com>
2023-04-18 12:11:51 -0700

    task

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -42,7 +42,7 @@ def matrix_mul(m_a, m_b):
     if not all(len(row) == len(m_b[0]) for row in m_b):
         raise TypeError("each row of m_b must should be of the same size")
 
-    if (len(m_a) != len(m_b[0]) or len(m_b) != len(m_a[0])):
+    if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
     inverted_b = []
