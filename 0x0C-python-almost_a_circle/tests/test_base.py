633c63fd65fda73d6e26f5809e47c1f2a22d5281
tedygreat <tedygreat@gmail.com>
2023-04-20 09:22:51 -0700

    task

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -45,56 +45,6 @@ class TestBase(unittest.TestCase):
         self.assertEqual(j_d[0]['id'], 8)
         self.assertEqual(j_d[1]['x'], 12)
 
-    def test_write_file_basic(self):
-        """tests write to file basic capabilities, given 1 type of class
-        """
-        s = Square(3, 1, 1, 10)
-        s2 = Square(4, 2, 2, 20)
-        r1 = Rectangle(5, 6, 3, 3, 30)
-        r2 = Rectangle(7, 8, 4, 4, 40)
-        Base.save_to_file([s, s2])
-        with open('Square.json', 'r', encoding='utf-8') as myFile:
-            text = myFile.read()
-        list_of_dicts = eval(text)
-        self.assertEqual(list_of_dicts[0]['id'], 10)
-        self.assertEqual(list_of_dicts[1]['x'], 2)
-
-        Base.save_to_file([r1, r2])
-        with open('Rectangle.json', 'r', encoding='utf-8') as myFile:
-            text = myFile.read()
-        list_of_dicts = eval(text)
-        self.assertEqual(list_of_dicts[0]['id'], 30)
-        self.assertEqual(list_of_dicts[1]['x'], 4)
-
-    def test_write_file_complex(self):
-        """tests writing a file with harder inputs
-        """
-        s = Square(3, 1, 1, 10)
-        s2 = Square(4, 2, 2, 20)
-        r1 = Rectangle(5, 6, 3, 3, 30)
-        r2 = Rectangle(7, 8, 4, 4, 40)
-        Base.save_to_file(["hello", 42, "more garb", True, s, s2])
-        with open('Square.json', 'r', encoding='utf-8') as myFile:
-            text = myFile.read()
-        list_of_dicts = eval(text)
-        self.assertEqual(list_of_dicts[0]['id'], 10)
-        self.assertEqual(list_of_dicts[1]['x'], 2)
-
-        Base.save_to_file([s, 89, r1, "garb", 42, s2, r2])
-        with open('Rectangle.json', 'r', encoding='utf-8') as myFile:
-            text = myFile.read()
-        list_of_dicts = eval(text)
-        self.assertEqual(list_of_dicts[1]['id'], 30)
-        self.assertEqual(list_of_dicts[3]['x'], 4)
-
-    def test_write_file_empty(self):
-        """tests empty list is written to correct default file
-        """
-        Base.save_to_file([])
-        with open('Rectangle.json', 'r', encoding='utf-8') as myFile:
-            text = myFile.read()
-        self.assertEqual(text, "[]")
-
     def test_from_json(self):
         """tests bases from_json_string method to convert string to
                 -> list of dictionaries
