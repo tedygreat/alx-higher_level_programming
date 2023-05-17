3206d99ecd18b8905c3f35456094dd4f00a089f1
tedygreat <tedygreat@gmail.com>
2023-05-17 10:53:34 -0700

    task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- script for selecting TV shows and their genres
--- join statement to do result DML
+-- join statement to do result DML.
 SELECT
   tv_genres.name AS genre,
   COUNT(tv_show_genres.genre_id) AS number_of_shows
