20e7b04c4c49da95ad239b29824c9222c6bc2b59
tedygreat <tedygreat@gmail.com>
2023-05-17 10:51:18 -0700

    task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- script for selecting TV shows and their genres
--- join statement to do result DML
+-- join statement to do result DML.
 SELECT tv_shows.title, tv_show_genres.genre_id
   FROM tv_shows LEFT JOIN tv_show_genres
   ON tv_shows.id = tv_show_genres.show_id
