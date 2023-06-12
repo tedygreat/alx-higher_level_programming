e663f35d5d9ca84588cd2dcb24e65a354a4b9ae6
tedygreat <tedygreat@gmail.com>
2023-06-12 12:57:47 -0700

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-// prints the first argument passed to
+// prints the first argument passed to it
 
-const process = require('process');
-let arg = process.argv[2];
-if (arg === undefined) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(arg);
+  console.log(process.argv[2]);
 }
