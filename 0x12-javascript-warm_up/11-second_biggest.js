37f7a563d7340e3468c81995dfeeb9b97c1f43b1
tedygreat <tedygreat@gmail.com>
2023-06-12 12:34:46 -0700

    task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,23 +1,9 @@
 #!/usr/bin/node
-const process = require('process');
-let max;
-let secondMax;
+// searches the second biggest integer in the list of arguments.
 
-process.argv.forEach(function (value, index) {
-  value = parseInt(value);
-  if (index > 1) {
-    if (max === undefined) {
-      max = value;
-    } else if (secondMax === undefined && value <= max) {
-      secondMax = value;
-    } else if (value >= max) {
-      secondMax = max;
-      max = value;
-    }
-  }
-});
-if (secondMax === undefined) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  console.log(secondMax);
+  const list = process.argv.sort();
+  console.log(list.reverse()[1]);
 }
