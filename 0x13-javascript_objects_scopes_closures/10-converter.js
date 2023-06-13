7f13bdeb6d700491706a7b717b74b08065515e02
tedygreat <tedygreat@gmail.com>
2023-06-13 12:45:02 -0700

    task 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,2 @@
 #!/usr/bin/node
-
-exports.converter = function converter (base) {
-  if (typeof converter.Num === 'undefined') {
-    converter.Num = base;
-    return converter;
-  } else {
-    return parseInt(converter.Num, base);
-  }
-};
+exports.converter = function (base) { return num => num.toString(base); };
