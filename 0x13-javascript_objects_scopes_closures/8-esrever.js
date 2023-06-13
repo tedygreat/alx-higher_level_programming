5cce8db64c235bdd0dfa17e673717bbafb1c7694
tedygreat <tedygreat@gmail.com>
2023-06-13 12:54:48 -0700

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let newList = [];
-  let i;
-  for (i = list.length - 1; i >= 0; i--) {
-    newList.push(list[i]);
-  }
-  return newList;
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
