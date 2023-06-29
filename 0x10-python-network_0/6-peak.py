d7fd3851db8daafcf9e2368f0fae7f4f5b01932d
tedygreat <tedygreat@gmail.com>
2023-06-29 14:41:55 -0700

    t6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -17,7 +17,7 @@ def find_peak(list_of_integers):
     peak = list_of_integers[mid]
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
-    elif peak < list_of_integers[mid - 1]:
+    elif peak <= list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
     else:
         return find_peak(list_of_integers[mid + 1:])
